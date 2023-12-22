c3f2d3310f89c9cc5d6fa0c8a15adb961b2ebdb2
ytir <ytir@dept-info.crosemont.quebec>
2023-12-22 11:39:02 -0500

    Implémentation Chat

--- CrossConvo/CrossConvo/wwwroot/js/site.js
+++ CrossConvo/CrossConvo/wwwroot/js/site.js
@@ -1,43 +1,72 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
-
-// Write your JavaScript code.
-// Création d'une nouvelle instance de HubConnectionBuilder
+﻿// Crée une nouvelle connexion SignalR en utilisant la configuration spécifiée
 const connection = new signalR.HubConnectionBuilder()
-    .withUrl("/learningHub") // Spécification de l'URL du hub SignalR
-    .configureLogging(signalR.LogLevel.Information) // Configuration du niveau de
-journalisation
-    .build(); // Construction de l'objet HubConnection
-// Définition d'une fonction de réception des messages du hub
+    .withUrl("/learningHub") // Configure l'URL du hub
+    .configureLogging(signalR.LogLevel.Information) // Configure le niveau de journalisation
+    .build();
+
+// Définit une méthode pour traiter les messages reçus du hub
 connection.on("ReceiveMessage", (message) => {
-    // Prépend le message reçu à un élément dans le DOM avec l'id 'signalr-messagepanel'
     $('#signalr-message-panel').prepend($('<div />').text(message));
 });
-// Gestionnaire d'événement pour le clic sur le bouton avec l'id 'btn-broadcast'
+
+// Associe une fonction de rappel au clic du bouton avec l'ID 'btn-broadcast'
 $('#btn-broadcast').click(function () {
-    // Récupération du message à diffuser depuis un élément avec l'id 'broadcast'
     var message = $('#broadcast').val();
-    // Invocation de la méthode côté serveur "BroadcastMessage" avec le message
-    spécifié
-    connection.invoke("BroadcastMessage", message).catch(err =>
-        console.error(err.toString()));
+    // Invoque la méthode côté serveur 'BroadcastMessage' avec le message spécifié
+    connection.invoke("BroadcastMessage", message).catch(err => console.error(err.toString()));
 });
-// Fonction asynchrone pour démarrer la connexion
+
+// Associe une fonction de rappel au clic du bouton avec l'ID 'btn-others-message'
+$('#btn-others-message').click(function () {
+    var message = $('#others-message').val();
+    // Invoque la méthode côté serveur 'SendToOthers' avec le message spécifié
+    connection.invoke("SendToOthers", message).catch(err => console.error(err.toString()));
+});
+
+// Associe une fonction de rappel au clic du bouton avec l'ID 'btn-self-message'
+$('#btn-self-message').click(function () {
+    var message = $('#self-message').val();
+    // Invoque la méthode côté serveur 'SendToCaller' avec le message spécifié
+    connection.invoke("SendToCaller", message).catch(err => console.error(err.toString()));
+});
+
+// Associe une fonction de rappel au clic du bouton avec l'ID 'btn-group-message'
+$('#btn-group-message').click(function () {
+    var message = $('#group-message').val();
+    var group = $('#group-for-message').val();
+    // Invoque la méthode côté serveur 'SendToGroup' avec le groupe et le message spécifiés
+    connection.invoke("SendToGroup", group, message).catch(err => console.error(err.toString()));
+});
+
+// Associe une fonction de rappel au clic du bouton avec l'ID 'btn-group-add'
+$('#btn-group-add').click(function () {
+    var group = $('#group-to-add').val();
+    // Invoque la méthode côté serveur 'AddUserToGroup' avec le groupe spécifié
+    connection.invoke("AddUserToGroup", group).catch(err => console.error(err.toString()));
+});
+
+// Associe une fonction de rappel au clic du bouton avec l'ID 'btn-group-remove'
+$('#btn-group-remove').click(function () {
+    var group = $('#group-to-remove').val();
+    // Invoque la méthode côté serveur 'RemoveUserFromGroup' avec le groupe spécifié
+    connection.invoke("RemoveUserFromGroup", group).catch(err => console.error(err.toString()));
+});
+
+// Fonction asynchrone pour démarrer la connexion SignalR
 async function start() {
     try {
-        // Tentative de démarrage de la connexion
-        await connection.start();
-        console.log('connected'); // Affichage dans la console si la connexion est
-        établie
+        await connection.start(); // Tente de démarrer la connexion
+        console.log('connected');
     } catch (err) {
-        console.log(err); // Affichage dans la console en cas d'erreur de connexion
-        setTimeout(() => start(), 5000); // Nouvelle tentative de connexion après 5
-secondes en cas d'échec
+        console.log(err);
+        setTimeout(() => start(), 5000); // En cas d'échec, réessaie après 5 secondes
     }
 }
-// Gestionnaire d'événement pour la fermeture de la connexion
+
+// Gère l'événement de fermeture de la connexion en redémarrant la connexion
 connection.onclose(async () => {
-    await start(); // Redémarrage de la connexion en cas de fermeture
+    await start();
 });
-// Démarrage initial de la connexion
+
+// Démarre la connexion lors du chargement de la page
 start();
---
+++ CrossConvo/DotnetClient/Program.cs
@@ -0,0 +1,107 @@
+﻿using Microsoft.AspNetCore.SignalR.Client;
+
+// Demande à l'utilisateur de spécifier l'URL du SignalR Hub
+Console.WriteLine("Veuillez spécifier l'URL du SignalR Hub");
+var url = Console.ReadLine();
+
+// Crée une connexion au hub SignalR en utilisant l'URL spécifiée
+var hubConnection = new HubConnectionBuilder()
+                         .WithUrl(url)
+                         .Build();
+
+// Configure une méthode pour traiter les messages reçus du hub
+hubConnection.On<string>("ReceiveMessage",
+    message => Console.WriteLine($"Message du SignalR Hub : {message}"));
+
+try
+{
+    // Tente de démarrer la connexion au hub SignalR
+    await hubConnection.StartAsync();
+
+    var running = true;
+
+    while (running)
+    {
+        var message = string.Empty;
+
+        var groupName = string.Empty;
+
+        // Affiche les options d'action disponibles
+        Console.WriteLine("Veuillez spécifier l'action :");
+        Console.WriteLine("0 - diffuser à tous");
+        Console.WriteLine("1 - envoyer aux autres");
+        Console.WriteLine("2 - envoyer à soi-même");
+        Console.WriteLine("3 - envoyer à un individu");
+        Console.WriteLine("4 - envoyer à un groupe");
+        Console.WriteLine("5 - ajouter un utilisateur à un groupe");
+        Console.WriteLine("6 - supprimer un utilisateur d'un groupe");
+        Console.WriteLine("exit - Quitter le programme");
+
+        // Lit l'action spécifiée par l'utilisateur
+        var action = Console.ReadLine();
+
+        // Si l'action n'est pas liée à l'ajout ou à la suppression d'un utilisateur d'un groupe, demande le message
+        if (action != "5" && action != "6")
+        {
+            Console.WriteLine("Veuillez spécifier le message :");
+            message = Console.ReadLine();
+        }
+
+        // Si l'action est liée à l'envoi à un groupe, à l'ajout ou à la suppression d'un utilisateur d'un groupe, demande le nom du groupe
+        if (action == "4" || action == "5" || action == "6")
+        {
+            Console.WriteLine("Veuillez spécifier le nom du groupe :");
+            groupName = Console.ReadLine();
+        }
+
+        // Exécute l'action spécifiée
+        switch (action)
+        {
+            case "0":
+                // Diffuse le message à tous les utilisateurs
+                await hubConnection.SendAsync("BroadcastMessage", message);
+                break;
+            case "1":
+                // Envoie le message aux autres utilisateurs
+                await hubConnection.SendAsync("SendToOthers", message);
+                break;
+            case "2":
+                // Envoie le message à soi-même
+                await hubConnection.SendAsync("SendToCaller", message);
+                break;
+            case "3":
+                // Demande l'ID de connexion et envoie le message à un utilisateur individuel
+                Console.WriteLine("Veuillez spécifier l'ID de connexion :");
+                var connectionId = Console.ReadLine();
+                await hubConnection.SendAsync("SendToIndividual", connectionId, message);
+                break;
+            case "4":
+                // Envoie le message à un groupe spécifié
+                hubConnection.SendAsync("SendToGroup", groupName, message).Wait();
+                break;
+            case "5":
+                // Ajoute l'utilisateur à un groupe spécifié
+                hubConnection.SendAsync("AddUserToGroup", groupName).Wait();
+                break;
+            case "6":
+                // Supprime l'utilisateur d'un groupe spécifié
+                hubConnection.SendAsync("RemoveUserFromGroup", groupName).Wait();
+                break;
+            case "exit":
+                // Quitte le programme
+                running = false;
+                break;
+            default:
+                Console.WriteLine("Action non valide spécifiée");
+                break;
+        }
+    }
+}
+catch (Exception ex)
+{
+    // En cas d'erreur, affiche le message d'erreur et attend une touche pour quitter
+    Console.WriteLine(ex.Message);
+    Console.WriteLine("Appuyez sur n'importe quelle touche pour quitter...");
+    Console.ReadKey();
+    return;
+}
